70e8e357ccf5e99675ffe1fdf1d6c8cda4386ff0
Dev&+ <45708451+Eliftb186@users.noreply.github.com>
2023-05-15 11:55:41 +0200

    Création d'une liste random

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 # Jefferson-Cylinder Par William BERGUE & Elias MOUSSA - SupInfo 2022/2023 - BenG1
 
+import random
 alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
+def creerCylindre():
+    # Création d'un cylindre avec les lettres de l'alphabet dans un ordre aléatoire
+    cylindre = []
+    for i in range(0, 26):
+        cylindre.append(alphabet[i])
+    random.shuffle(cylindre)
+    return cylindre
+
+print(creerCylindre())
+print(creerCylindre())
 
 
